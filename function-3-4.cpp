537f13c0e9a7c5d5763fc57e2b80362d83afe52a
ZiYi Wang <ziyi.wang03@student.adelaide.edu.au>
2025-03-07 23:39:39 +1030

    Add files via upload

---
+++ function-3-4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+void print_pass_fail(char grade){
+    switch(grade){
+        case 'A':
+        case 'B':
+        case 'C':
+            std::cout<<"pass\n";
+            break;
+        case 'D':
+        case 'E':
+            std::cout << "Fail\n";
+            break;
+        default:
+            std::cout << "Nothing\n";
+            break;
+    }
+}
